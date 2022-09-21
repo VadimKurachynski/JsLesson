4d583c368c12b89de51cf673f343fb8190e22467
VadimKurachynski <kurachynski@gmail.com>
2022-09-21 23:16:27 +0300

    5

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("привет!");
+// console.log(6475+6);
+
+document.getElementById('out').innerHTML='233';
+document.querySelector("div").innerHTML="444";
+document.querySelector(".out1").innerHTML="666";
+
