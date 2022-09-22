d2c06f9e1d9d51c0ac7aabbbb804eaef69aa92d0
VadimKurachynski <kurachynski@gmail.com>
2022-09-22 23:13:10 +0300

    5d

--- script.js
+++ script.js
@@ -1,22 +1,13 @@
 console.log("привет!");
-// let a=1;
-// let b=1;
+document.getElementById('out').innerHTML = '5555555';
+let a = document.querySelector('.out1').innerHTML;
+console.log(a);
+let b = document.querySelector('.out2').clientWidth;
+console.log(b);
+document.querySelector('.out2').innerHTML = b;
+let c = document.querySelector(".input1");
+let button1 = document.querySelector('.button1');
+button1.onclick = function () {
+    console.log(c.value);
 
-document.getElementById('out').innerHTML='5555555';
-
-//  let a=document.querySelector('#out1');
-//  console.log(a.innerHTML);
- console.log(document.querySelector('#out1'));
-
-
-
-
- let b=document.querySelector(".input1");
- let button1 =document.querySelector('.button1');
-
-
-
-button1.onclick=function(){
-    console.log(b);
-   
 }
