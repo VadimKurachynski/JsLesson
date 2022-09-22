f4dd319d6d44f7f7538594a5f5ad7659535c0cb9
VadimKurachynski <kurachynski@gmail.com>
2022-09-22 23:25:32 +0300

    5z

--- script.js
+++ script.js
@@ -9,5 +9,9 @@ let c = document.querySelector(".input1");
 let button1 = document.querySelector('.button1');
 button1.onclick = function () {
     console.log(c.value);
+}
 
+let button2 = document.querySelector(".buttonU3");
+button2.onclick = ()=> {
+    console.log(c.value);
 }
