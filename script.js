8c6f2e503a7cae3fad6d8649322e8430d8dcda30
VadimKurachynski <kurachynski@gmail.com>
2022-09-22 22:31:51 +0300

    3a

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 console.log("привет!");
-document.getElementById('out').innerHTML='233';
-document.querySelector("div").innerHTML="444";
+let a=1;
 
-let a=document.querySelector("unit unit2.out1").textContent;
-console.log(a);
+let b=1;
+// document.getElementById('out').innerHTML='233';
+// document.querySelector("div").innerHTML="444";
 
- let b=document.querySelector(".input1");
-let button1 =document.querySelector('.button1');
+// let a=document.querySelector("unit unit2.out1").textContent;
+// console.log(a);
+
+//  let b=document.querySelector(".input1");
+// let button1 =document.querySelector('.button1');
 
 
 button1.onclick=function(){
