558284d249d7d693de0fa191682012e537fd997a
VadimKurachynski <kurachynski@gmail.com>
2022-09-22 22:22:42 +0300

    1d

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 console.log("привет!");
-// console.log(6475+6);
-
 document.getElementById('out').innerHTML='233';
 document.querySelector("div").innerHTML="444";
-document.querySelector(".out1").innerHTML="666";
 
+let a=document.querySelector("unit unit2.out1").textContent;
+console.log(a);
+
+ let b=document.querySelector(".input1");
+let button1 =document.querySelector('.button1');
+
+
+button1.onclick=function(){
+   
+    console.log(+b.value+1);
+}
