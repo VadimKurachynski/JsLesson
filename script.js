f3c4a7a1ec8b969da46791a245f9398a22743784
VadimKurachynski <kurachynski@gmail.com>
2022-09-23 17:48:27 +0300

    5d

--- script.js
+++ script.js
@@ -36,4 +36,14 @@ document.querySelector(".inputU41").oninput=()=>{
    document.querySelector(".u41").innerHTML=
    document.querySelector(".inputU41").value;
 }
+document.querySelector(".inputU42").oninput=()=>{
+   if(document.querySelector(".inputU42").checked){
+      document.querySelector(".u42").innerHTML=
+   "true";
+   }else{
+      document.querySelector(".u42").innerHTML=
+   "true";
+   }
+   
 
+}
