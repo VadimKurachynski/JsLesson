1db6d174a5c1f7da7d6f90f9e6f10f2ae6c84b5a
VadimKurachynski <kurachynski@gmail.com>
2022-10-02 00:13:40 +0300

    1w

--- first-servers/hello.js
+++ first-servers/hello.js
@@ -2,14 +2,22 @@ const http = require("http");
 const host = 'localhost';
 const port = 8000;
 
-let ghj=78;
+let g = {a1:5,
+a2:6,
+a3:7,
+};
+g=JSON.stringify(g);
 const requestListener = function (req, res) {
+    res.setHeader("Content-Type", "text/csv");
+    // res.setHeader("Content-Disposition", "attachment;filename=oceanpals.csv");
     res.setHeader("Content-Type", "application/json");
     res.writeHead(200);
-    res.end(`{"message": "This is a JSON response"}`);
+    res.end(`${g}`);
+    // res.end(`{"message": "This is a JSON response"}`);
     // res.end(`My first server!${ghj}`);
 };
 const server = http.createServer(requestListener);
 server.listen(port, host, () => {
     console.log(`Server is running on http://${host}:${port}`);
+   
 });
