a572193d1493c9dd9ce2369ee3a7b618e76751bd
VadimKurachynski <kurachynski@gmail.com>
2022-10-02 17:11:16 +0300

    3d

--- first-servers/hello.js
+++ first-servers/hello.js
@@ -21,5 +21,4 @@ const requestListener = function (req, res) {
 const server = http.createServer(requestListener);
 server.listen(port, host, () => {
     console.log(`Server is running on http://${host}:${port}`);
-
 });
