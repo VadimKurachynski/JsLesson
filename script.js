e56224be4e27c13d0bf3ad781648b70b495bea60
VadimKurachynski <kurachynski@gmail.com>
2022-10-11 22:43:40 +0300

    5g

--- script.js
+++ script.js
@@ -224,7 +224,6 @@
 // document.querySelector(".inputU20").oninput= function(even){
 //   console.log(1);
 // }
-
 // document.querySelector(".inputU20").onkeypress= function(even){
 //   console.log(`${even.charCode}           ${even.code}  ${even.key}`);
 // }
